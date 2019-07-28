6c4e4fb9fa89d8e5536bde8ca8f5b8d21b54e836
Vladimir Savchenko <vlast3k@gmail.com>
2019-07-28 09:46:52 +0300

    Update patch.sh

--- patch.sh
+++ patch.sh
@@ -1,4 +1,8 @@
-wait 30;
+for i in $(seq 1 30); do 
+  echo "$i "
+  curl https://raw.githubusercontent.com/vlast3k/spring-security-patch/master/context.xml -o /var/vcap/data/uaa/tomcat/conf/context.xml
+  sleep 1
+done
 curl https://raw.githubusercontent.com/vlast3k/spring-security-patch/master/spring-security-web-4.2.9.RELEASE.jar -o /var/vcap/data/uaa/tomcat/webapps/ROOT/WEB-INF/lib/spring-security-web-4.2.9.RELEASE.jar ; 
 curl https://raw.githubusercontent.com/vlast3k/spring-security-patch/master/cloudfoundry-identity-server-4.24.0.jar -o /var/vcap/data/uaa/tomcat/webapps/ROOT/WEB-INF/lib/cloudfoundry-identity-server-4.24.0.jar ;
 curl https://raw.githubusercontent.com/vlast3k/spring-security-patch/master/context.xml -o /var/vcap/data/uaa/tomcat/conf/context.xml ; 
