85e586ea486b21d113f063ccf36003d3882ca3e9
Vladimir Savchenko <vlast3k@gmail.com>
2019-07-28 08:53:08 +0300

    Add files via upload

--- patch.sh
+++ patch.sh
@@ -2,4 +2,4 @@ curl https://raw.githubusercontent.com/vlast3k/spring-security-patch/master/spri
 curl https://raw.githubusercontent.com/vlast3k/spring-security-patch/master/cloudfoundry-identity-server-4.24.0.jar -o /var/vcap/data/uaa/tomcat/webapps/ROOT/WEB-INF/lib/cloudfoundry-identity-server-4.24.0.jar
 curl https://raw.githubusercontent.com/vlast3k/spring-security-patch/master/context.xml -o /var/vcap/data/uaa/tomcat/conf/context.xml
 curl https://raw.githubusercontent.com/vlast3k/spring-security-patch/master/server.xml -o /var/vcap/data/uaa/tomcat/conf/server.xml
-monit restart uaa
+monit restart uaa 
