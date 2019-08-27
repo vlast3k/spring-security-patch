aa66755acaea96c2246fb558bfb03aa4433c9cbc
Vladimir Savchenko <vlast3k@gmail.com>
2019-08-27 05:13:35 +0300

    Update patch-3.2.0.sh

--- patch-3.2.0.sh
+++ patch-3.2.0.sh
@@ -12,5 +12,6 @@ curl https://raw.githubusercontent.com/vlast3k/spring-security-patch/master/clou
 curl https://raw.githubusercontent.com/vlast3k/spring-security-patch/master/context.xml -o /var/vcap/data/uaa/tomcat/conf/context.xml  	
 curl https://raw.githubusercontent.com/vlast3k/spring-security-patch/master/server.xml -o /var/vcap/data/uaa/tomcat/conf/server.xml  	
 curl https://raw.githubusercontent.com/vlast3k/spring-security-patch/master/uaa -o /var/vcap/jobs/uaa/bin/uaa	
+curl https://raw.githubusercontent.com/vlast3k/tomcat/master/custom-tomcat-utils/target/custom-tomcat-utils-0.0.1-SNAPSHOT.jar -o /var/vcap/data/uaa/tomcat/lib/custom-tomcat-utils-0.0.1-SNAPSHOT.jar
 chmod 777 /var/vcap/jobs/uaa/bin/uaa	
 /var/vcap/bosh/bin/monit restart uaa
