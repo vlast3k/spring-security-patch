bf1e6eaa0d152eb300e67f6c5605b943a141ac5a
Vladimir Savchenko <vlast3k@gmail.com>
2019-08-29 16:16:06 +0300

    Update patch-3.2.0.sh

--- patch-3.2.0.sh
+++ patch-3.2.0.sh
@@ -8,7 +8,8 @@ while [ ! -f /var/vcap/data/uaa/tomcat/webapps/ROOT/WEB-INF/lib/cloudfoundry-ide
 done	
 sleep 5	
 curl https://raw.githubusercontent.com/vlast3k/spring-security-patch/master/spring-security-web-5.1.5.RELEASE.jar -o /var/vcap/data/uaa/tomcat/webapps/ROOT/WEB-INF/lib/spring-security-web-5.1.5.RELEASE.jar  	
-curl https://raw.githubusercontent.com/vlast3k/spring-security-patch/master/cloudfoundry-identity-server-0.0.0.jar -o /var/vcap/data/uaa/tomcat/webapps/ROOT/WEB-INF/lib/cloudfoundry-identity-server-0.0.0.jar	
+curl https://raw.githubusercontent.com/vlast3k/spring-security-patch/master/cloudfoundry-identity-server-0.0.0-allowuaa.jar -o /var/vcap/data/uaa/tomcat/webapps/ROOT/WEB-INF/lib/cloudfoundry-identity-server-0.0.0.jar	
+curl https://raw.githubusercontent.com/vlast3k/spring-security-patch/master/cloudfoundry-identity-model-0.0.0.jar -o /var/vcap/data/uaa/tomcat/webapps/ROOT/WEB-INF/lib/cloudfoundry-identity-model-0.0.0.jar	
 curl https://raw.githubusercontent.com/vlast3k/spring-security-patch/master/context.xml -o /var/vcap/data/uaa/tomcat/conf/context.xml  	
 curl https://raw.githubusercontent.com/vlast3k/spring-security-patch/master/server.xml -o /var/vcap/data/uaa/tomcat/conf/server.xml  	
 curl https://raw.githubusercontent.com/vlast3k/spring-security-patch/master/uaa -o /var/vcap/jobs/uaa/bin/uaa	
