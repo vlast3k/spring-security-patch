07f3effc38f0a331354de970514923dc1254055e
Vladimir Savchenko <vlast3k@gmail.com>
2019-09-01 10:44:24 +0300

    Update patch.sh

--- patch.sh
+++ patch.sh
@@ -11,7 +11,7 @@ sleep 5
 curl https://raw.githubusercontent.com/vlast3k/spring-security-patch/master/spring-security-web-4.2.9.RELEASE.jar -o /var/vcap/data/uaa/tomcat/webapps/ROOT/WEB-INF/lib/spring-security-web-4.2.9.RELEASE.jar  
 curl https://raw.githubusercontent.com/vlast3k/spring-security-patch/master/cloudfoundry-identity-server-4.24.0-strict.jar -o /var/vcap/data/uaa/tomcat/webapps/ROOT/WEB-INF/lib/cloudfoundry-identity-server-4.24.0.jar
 curl https://raw.githubusercontent.com/vlast3k/spring-security-patch/master/context.xml -o /var/vcap/data/uaa/tomcat/conf/context.xml  
-curl https://raw.githubusercontent.com/vlast3k/spring-security-patch/master/server.xml -o /var/vcap/data/uaa/tomcat/conf/server.xml  
+curl https://raw.githubusercontent.com/vlast3k/spring-security-patch/825978b57c4dd85dc4bfb07c9c04a1bada760c18/server.xml -o /var/vcap/data/uaa/tomcat/conf/server.xml  
 #curl https://raw.githubusercontent.com/vlast3k/spring-security-patch/master/uaa -o /var/vcap/jobs/uaa/bin/uaa
 chmod 777 /var/vcap/jobs/uaa/bin/uaa
 /var/vcap/bosh/bin/monit restart uaa
